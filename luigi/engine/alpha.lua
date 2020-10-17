2a95312d8437025f7ed0e0cad703a11ab7add005
Mads Hvelplund <mhvelplund@gmail.com>
2020-10-17 13:49:56 +0200

    Removed duplicate attribute

--- luigi/engine/alpha.lua
+++ luigi/engine/alpha.lua
@@ -1,12 +1,12 @@
 local RESOURCE = (...):gsub('%.', '/') .. '/'
 
-return function (config)
+return function(config)
     config = config or {}
     local resources = assert(config.resources, 'missing config.resources')
-    local backColor = config.backColor or { 240, 240, 240 }
-    local lineColor = config.lineColor or { 220, 220, 220 }
-    local textColor = config.textColor or { 0, 0, 0 }
-    local highlight = config.highlight or { 0x19, 0xAE, 0xFF }
+    local backColor = config.backColor or {240, 240, 240}
+    local lineColor = config.lineColor or {220, 220, 220}
+    local textColor = config.textColor or {0, 0, 0}
+    local highlight = config.highlight or {0x19, 0xAE, 0xFF}
 
     local button_pressed = resources .. 'button_pressed.png'
     local button_focused = resources .. 'button_focused.png'
@@ -35,78 +35,72 @@ return function (config)
     local text_focused = resources .. 'text_focused.png'
     local text = resources .. 'text.png'
 
-    local function getButtonSlices (self)
-        return self.pressed.left and button_pressed
-            or self.focused and button_focused
-            or self.hovered and button_hovered
-            or button
+    local function getButtonSlices(self)
+        return self.pressed.left and button_pressed or self.focused and button_focused or self.hovered and
+                   button_hovered or button
     end
 
-    local function getCheckIcon (self)
+    local function getCheckIcon(self)
         if self.pressed.left then
-            return self.value and check_checked_pressed
-                or check_unchecked_pressed
+            return self.value and check_checked_pressed or check_unchecked_pressed
         end
         if self.focused then
-            return self.value and check_checked_focused
-                or check_unchecked_focused
+            return self.value and check_checked_focused or check_unchecked_focused
         end
         return self.value and check_checked or check_unchecked
     end
 
-    local function getControlHeight (self)
+    local function getControlHeight(self)
         return self.flow == 'x' and self._defaultDimension
     end
 
-    local function getControlWidth (self)
+    local function getControlWidth(self)
         return self.flow ~= 'x' and self._defaultDimension
     end
 
-    local function getMenuItemBackground (self)
+    local function getMenuItemBackground(self)
         return self.active and highlight
     end
 
-    local function getRadioIcon (self)
+    local function getRadioIcon(self)
         if self.pressed.left then
-            return self.value and radio_checked_pressed
-                or radio_unchecked_pressed
+            return self.value and radio_checked_pressed or radio_unchecked_pressed
         end
         if self.focused then
-            return self.value and radio_checked_focused
-                or radio_unchecked_focused
+            return self.value and radio_checked_focused or radio_unchecked_focused
         end
         return self.value and radio_checked or radio_unchecked
     end
 
-    local function getSashBackground (self)
+    local function getSashBackground(self)
         return self.hovered and highlight or lineColor
     end
 
-    local function getSashHeight (self)
+    local function getSashHeight(self)
         return self.parent and self.parent.flow ~= 'x' and 4
     end
 
-    local function getSashWidth (self)
+    local function getSashWidth(self)
         return self.parent and self.parent.flow == 'x' and 4
     end
 
-    local function getSliderThumbWidth (self)
+    local function getSliderThumbWidth(self)
         return self.parent.flow == 'x' and 32 or false
     end
 
-    local function getSliderThumbHeight (self)
+    local function getSliderThumbHeight(self)
         return self.parent.flow ~= 'x' and 32 or false
     end
 
-    local function getStepperBeforeIcon (self)
+    local function getStepperBeforeIcon(self)
         return self.parent.flow == 'x' and triangle_left or triangle_up
     end
 
-    local function getStepperAfterIcon (self)
+    local function getStepperAfterIcon(self)
         return self.parent.flow == 'x' and triangle_right or triangle_down
     end
 
-    local function getTextSlices (self)
+    local function getTextSlices(self)
         return self.focused and text_focused or text
     end
 
@@ -121,131 +115,130 @@ return function (config)
             color = textColor,
             align = 'center middle',
             margin = 2,
-            color = textColor,
             solid = true,
-            _defaultDimension = 36,
+            _defaultDimension = 36
         },
 
         Line = {
             margin = 0,
             padding = 4,
             align = 'left middle',
-            _defaultDimension = 24,
+            _defaultDimension = 24
         },
 
         -- widget types
 
         button = {
-            type = { 'Control' },
+            type = {'Control'},
             padding = 6,
             slices = getButtonSlices,
-            focusable = true,
+            focusable = true
         },
         check = {
-            type = { 'Line', 'Control' },
+            type = {'Line', 'Control'},
             focusable = true,
-            icon = getCheckIcon,
+            icon = getCheckIcon
         },
         label = {
-            type = { 'Line', 'Control' },
+            type = {'Line', 'Control'}
         },
         menu = {
             flow = 'x',
             height = 24,
             background = backColor,
-            color = textColor,
+            color = textColor
         },
         ['menu.expander'] = {
-            icon = resources .. 'triangle_right.png',
+            icon = resources .. 'triangle_right.png'
         },
         ['menu.item'] = {
             padding = 4,
             height = 24,
             align = 'left middle',
-            background = getMenuItemBackground,
+            background = getMenuItemBackground
         },
         panel = {
             padding = 2,
             background = backColor,
             color = textColor,
-            solid = true,
+            solid = true
         },
         progress = {
-            type = { 'Control' },
-            slices = resources .. 'button_pressed.png',
+            type = {'Control'},
+            slices = resources .. 'button_pressed.png'
         },
         ['progress.bar'] = {
             slices = resources .. 'progress.png',
             minwidth = 12,
-            minheight = 22,
+            minheight = 22
         },
         radio = {
-            type = { 'Line', 'Control' },
+            type = {'Line', 'Control'},
             focusable = true,
-            icon = getRadioIcon,
+            icon = getRadioIcon
         },
         sash = {
             background = getSashBackground,
             height = getSashHeight,
-            width = getSashWidth,
+            width = getSashWidth
         },
         slider = {
-            type = { 'Control' },
-            slices = resources .. 'button_pressed.png',
+            type = {'Control'},
+            slices = resources .. 'button_pressed.png'
         },
         ['slider.thumb'] = {
-            type = { 'button' },
+            type = {'button'},
             align = 'middle center',
             margin = 0,
             width = getSliderThumbWidth,
-            height = getSliderThumbHeight,
+            height = getSliderThumbHeight
         },
         status = {
-            type = { 'Line', 'Control' },
+            type = {'Line', 'Control'},
             background = backColor,
-            color = textColor,
+            color = textColor
         },
         stepper = {
-            type = { 'Control' },
-            slices = resources .. 'button_pressed.png',
+            type = {'Control'},
+            slices = resources .. 'button_pressed.png'
         },
         ['stepper.after'] = {
-            type = { 'button' },
+            type = {'button'},
             icon = getStepperAfterIcon,
             margin = 0,
             minwidth = 32,
-            minheight = 32,
+            minheight = 32
         },
         ['stepper.before'] = {
-            type = { 'button' },
+            type = {'button'},
             icon = getStepperBeforeIcon,
             margin = 0,
             minwidth = 32,
-            minheight = 32,
+            minheight = 32
         },
         ['stepper.item'] = {
             align = 'center middle',
-            color = textColor,
+            color = textColor
         },
         ['stepper.view'] = {
-            margin = 4,
+            margin = 4
         },
         submenu = {
             padding = 10,
             margin = -10,
             slices = resources .. 'submenu.png',
             color = textColor,
-            solid = true,
+            solid = true
         },
         text = {
-            type = { 'Control' },
+            type = {'Control'},
             align = 'left middle',
             slices = getTextSlices,
             padding = 6,
             focusable = true,
             cursor = 'ibeam',
-            highlight = highlight,
-        },
+            highlight = highlight
+        }
     }
 
 end
